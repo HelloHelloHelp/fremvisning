c8500d4e1001fa1396940e978c0cd6d3c53f80e3
Zamzam Ali <zamali@pc-tec.dk>
2025-08-15 10:02:46 +0200

    first commit

---
+++ CV.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>CSS Website Layout</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+}
+.column {
+  float: left;
+  width: 45%;
+  padding: 15px;
+}
+/* Style the header */
+.header {
+  background-color: #f1f1f1;
+  padding: 20px;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<div class="header">
+  <h1>CV</h1>
+</div>
+<div class="row">
+  <div class="column">
+    <h2>Column</h2>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo  luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+    <p>          </p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo  luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+ <p>          </p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo  luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+ <p>          </p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo  luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+ <p>          </p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo  luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+ 
+  </div>
+  
+  
+  <div class="row">
+  <div class="column">
+    <h2>Column</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo  luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+ <p>          </p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo  luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+ <p>          </p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo  luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+ <p>          </p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo  luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+ <p>          </p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo  luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+ 
+  </div>
+  <body>
+</body>
+ <html>
+</html>
